134cf5513577e5b4caf8b4f03f1cc84f486de0fe
bvalette <bvalette@student.42.fr>
2021-08-30 12:01:24 +0200

    [ Mon-30.08-12:01 ] [DEV] setting closer to webserv

--- 01_start.sh
+++ 01_start.sh
@@ -2,7 +2,7 @@
 
 VOLUME=$(ls -d $PWD/volume)
 SUPERVISOR=$(ls -d $PWD/supervisor)
-docker run --rm -d -it -p 22:22 -p17000:17000 -p 80:80 -p 8080:8080 -p443:443 -i --mount type=bind,src=$VOLUME,dst=/tmp/volume --mount type=bind,src=$SUPERVISOR,dst=/tmp/supervisor bv/nginx_webserv
+docker run --rm -d -it -p 42:22 -p17000:17000 -p 80:80 -p 8080:8080 -p443:443 -i --mount type=bind,src=$VOLUME,dst=/tmp/volume --mount type=bind,src=$SUPERVISOR,dst=/tmp/supervisor bv/nginx_webserv
 echo "Ip of the new container is: "
 export CONTAINER=$(docker ps --filter "label=service=nginx" -q)
 docker inspect -f "{{ .NetworkSettings.IPAddress }}" $(docker ps --filter "label=service=nginx" -q)
